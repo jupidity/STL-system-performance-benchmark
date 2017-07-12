a698577b837cb3dde479a97811ed335c7f87a6f4
Sean Cassero <sacassero@gmail.com>
2017-07-12 13:57:50 -0400

    added compiler optomization and directory cleanup

--- compile.sh
+++ compile.sh
@@ -1,1 +1,17 @@
-g++ -std=c++11 -O2 main.cpp -o main
+#!/bin/bash
+
+# shell script for specifying compile directives to test STL container iterators against
+# Author: Sean Cassero
+
+# call with argument 1
+# ie ./compile.sh 1
+
+
+# compile with the proper directive
+g++ -std=c++11 -O2 timing_sequence_containers.cpp -o main
+
+# run the executable
+./main
+
+# clean the current directory
+rm main
